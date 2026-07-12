e4fdbe1372c85386a9368386a5ccfd6076b0c557
hanzala bawany <hanzalabawany0@gmail.com>
2026-07-13 01:10:58 +0500

    profile page integrattion

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -2,10 +2,15 @@ import s from "./profilePage.module.scss"
 import { userData } from '../../utills/dummyData'
 import ProfileItemsList from "../../components/profileItemsList/ProfileItemsList"
 import MessagesAndText from "../../components/messagesAndText/MessagesAndText"
+import { useSelector } from "react-redux";
+import noAvatar from "../../assets/avatar.png"
+import { useState } from "react";
 
 const ProfilePage = () => {
 
-  const loginUserData = JSON.parse(localStorage.getItem("user"));
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+
+  const loginUserData = useSelector((state) => state?.authReducer?.user);
   console.log(loginUserData, "login user data in profile page");
 
   
@@ -20,24 +25,24 @@ const ProfilePage = () => {
           <div className={s.sameConatiner}>
             <span>user information</span>
 
-            <button>update profile</button>
+            <button onClick={() => setShowUpdateModal(true)} >update profile</button>
           </div>
 
           <div className={s.userInfo}>
 
             <div className={s.userPic}>
               <span>profile pic :</span>
-              <img src="https://www.bing.com/th/id/OIP.5naUK-L8A7RxxF7JB0NLuAHaE8?w=251&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2" alt="" />
+              <img src={loginUserData?.avatar ? loginUserData?.avatar : noAvatar} alt="" />
             </div>
 
             <div className={s.userName}>
               <span >Username :</span>
-              <p className={s.userNameP} >{userData.name}</p>
+              <p className={s.userNameP} >{loginUserData?.userName}</p>
             </div>
 
             <div className={s.userEmail}>
               <span>UserEmail :</span>
-              <p>hanzalabawany0@gmail.com</p>
+              <p>{loginUserData?.email}</p>
             </div>
 
           </div>
